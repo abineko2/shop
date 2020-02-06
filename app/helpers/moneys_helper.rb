f0724bc513b069700e180c5f270b1553dfa3f57b
abineko2 <yuki1980426@gmail.com>
2020-02-06 20:16:40 +0900

    売り上げ日付並び修正

--- app/helpers/moneys_helper.rb
+++ app/helpers/moneys_helper.rb
@@ -1,6 +1,6 @@
 module MoneysHelper
   def dates
-      @moneys = Money.where('t_day >=?  and t_day <=?',@first_day,@last_day)
+      @moneys = Money.where('t_day >=?  and t_day <=?',@first_day,@last_day).order('t-day asc')
   end
 
   
